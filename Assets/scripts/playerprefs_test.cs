0e40cf5aaa5a10ccda185002e8f6d99dada51d0d
janhachoi <wnsgk7654@gmail.com>
2022-12-27 19:17:20 +0900

    12-27

--- Assets/scripts/playerprefs_test.cs
+++ Assets/scripts/playerprefs_test.cs
@@ -5,15 +5,24 @@ using UnityEngine.UI;
 
 public class playerprefs_test : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public InputField input_name;
+    public InputField input_age;
+    public InputField input_height;
 
-    // Update is called once per frame
-    void Update()
+
+    public void _save()
     {
-        
+        PlayerPrefs.SetString("name", input_name.text);
+        PlayerPrefs.SetInt("age", int.Parse(input_age.text));
+        PlayerPrefs.SetFloat("height", float.Parse(input_height.text));
+    }
+    public void _load()
+    {
+        if (PlayerPrefs.HasKey("name"))
+        {
+            input_name.text = PlayerPrefs.GetString("name");
+            input_age.text = PlayerPrefs.GetInt("age").ToString();
+            input_height.text = PlayerPrefs.GetFloat("height").ToString();
+        }
     }
 }
